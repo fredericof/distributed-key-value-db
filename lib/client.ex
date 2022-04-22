e1fa7aec37067b39caf985f6cd556de0f1d73f83
Frederico Bitencourt <fredericofbh@gmail.com>
2022-04-21 22:47:00 -0300

    Added GenServer registry and Client registry

---
+++ lib/client.ex
@@ -0,0 +1,24 @@
+defmodule KV.Client do
+  @doc """
+  Starts the registry.
+  """
+  def start_link(module, opts) do
+    GenServer.start_link(module, :ok, opts)
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  Ensures there is a bucket associated with the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+end
