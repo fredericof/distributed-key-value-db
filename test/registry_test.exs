f652072d358b2235a428af1c746dd5159d3fae44
Frederico Bitencourt <fredericofbh@gmail.com>
2022-04-25 20:29:10 -0300

    Change Bucket Implementation to ETS Erlang Lib

--- test/registry_test.exs
+++ test/registry_test.exs
@@ -1,39 +1,52 @@
-defmodule KV.RegistryTest do
+defmodule KV.EtsRegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
   test "spawns buckets", %{registry: registry} do
-    assert KV.Client.lookup(registry, "shopping") == :error
+    assert KV.Registry.lookup(registry, "shopping") == :error
 
-    KV.Client.create(registry, "shopping")
-    assert {:ok, bucket} = KV.Client.lookup(registry, "shopping")
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     KV.Bucket.put(bucket, "milk", 1)
     assert KV.Bucket.get(bucket, "milk") == 1
   end
 
   test "removes buckets on exit", %{registry: registry} do
-    KV.Client.create(registry, "shopping")
-    {:ok, bucket} = KV.Client.lookup(registry, "shopping")
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
-    assert KV.Client.lookup(registry, "shopping") == :error
+
+    # Do a call to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
+    assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
   test "removes bucket on crash", %{registry: registry} do
-    KV.Client.create(registry, "shopping")
-    {:ok, bucket} = KV.Client.lookup(registry, "shopping")
-
-    # Put a item and check
-    KV.Bucket.put(bucket, "eggs", 6)
-    assert KV.Bucket.get(bucket, "eggs") == 6
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     # Stop the bucket with non-normal reason
     Agent.stop(bucket, :shutdown)
-    assert KV.Client.lookup(registry, "shopping") == :error
+
+    # Do a call to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "bucket can crash at any time", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Simulate a bucket crash by explicitly and synchronously shutting it down
+    Agent.stop(bucket, :shutdown)
+
+    # Now trying to call the dead process causes a :noproc exit
+    catch_exit(KV.Bucket.put(bucket, "milk", 3))
   end
 
   test "are temporary workers" do
